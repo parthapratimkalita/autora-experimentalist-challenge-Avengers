fbf9fa31833b883fac4f08a0bb2d3989035549dc
parthapratimkalita <40621848+parthapratimkalita@users.noreply.github.com>
2024-08-05 06:48:52 +0000

    new updates: 1st 30% of the time random sampling and then do model comparision

--- src/autora/experimentalist/autora_experimentalist_example/__init__.py
+++ src/autora/experimentalist/autora_experimentalist_example/__init__.py
@@ -3,15 +3,19 @@ Example Experimentalist
 """
 import numpy as np
 import pandas as pd
+from autora.state import State, StandardState, on_state, estimator_on_state, Delta, VariableCollection
+from autora.experimentalist.falsification import falsification_sample
+from autora.experimentalist.model_disagreement import model_disagreement_sample
+from autora.experimentalist.uncertainty import uncertainty_sample
+from autora.experimentalist.random import random_pool, random_sample
 
 from typing import Union, List
 
 
 def sample(
-        conditions: Union[pd.DataFrame, np.ndarray],
+        all_conditions: Union[pd.DataFrame, np.ndarray],
         models: List,
-        reference_conditions: Union[pd.DataFrame, np.ndarray],
-        num_samples: int = 1) -> pd.DataFrame:
+        num: int = 1) -> pd.DataFrame:
     """
     Add a description of the sampler here.
 
@@ -35,9 +39,16 @@ def sample(
         3
 
     """
-    if num_samples is None:
-        num_samples = conditions.shape[0]
+    # **** STATE WRAPPER FOR YOUR EXPERIMENTALIST ***
+    
+    conditions = model_disagreement_sample(
+          all_conditions,
+          models,
+          num_samples = 10
+        )
+    print("hello")
+    conditions = conditions.sample(n=1)
+        
+  
 
-    new_conditions = conditions
-
-    return new_conditions[:num_samples]
+    return Delta(conditions)
