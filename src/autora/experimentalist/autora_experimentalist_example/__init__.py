6dd22f03ff4aa18524b206590d30f0d55381a64f
Hannes Voss <hannes-voss95@web.de>
2024-08-27 20:37:33 +0200

    Fix tests

--- src/autora/experimentalist/autora_experimentalist_example/__init__.py
+++ src/autora/experimentalist/autora_experimentalist_example/__init__.py
@@ -25,20 +25,11 @@ def sample(
             Attention: `conditions` is a field of the standard state
         models: The sampler might use output from the theorist.
             Attention: `models` is a field of the standard state
-        reference_conditions: The sampler might use reference conditons
+        reference_conditions: The sampler might use reference conditions
         num_samples: number of experimental conditions to select
 
     Returns:
         Sampled pool of experimental conditions
-
-    *Optional*
-    Examples:
-        These examples add documentation and also work as tests
-        >>> example_sampler([1, 2, 3, 4])
-        1
-        >>> example_sampler(range(3, 10))
-        3
-
     """
     # **** STATE WRAPPER FOR YOUR EXPERIMENTALIST ***
     
