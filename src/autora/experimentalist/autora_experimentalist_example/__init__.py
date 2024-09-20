b12278aea0769fc7bcc7b2e7163a52895c462a20
Alexander Gerhard Ditz <159267896+AlexanderDitz@users.noreply.github.com>
2024-09-20 15:53:54 +0200

    Update __init__.py
    added comments to the code

--- src/autora/experimentalist/autora_experimentalist_example/__init__.py
+++ src/autora/experimentalist/autora_experimentalist_example/__init__.py
@@ -14,22 +14,29 @@ def sample(
         reference_conditions: Union[pd.DataFrame, np.ndarray] = None,
         num: int = 1) -> pd.DataFrame:
     """
-    Add a description of the sampler here.
+        Samples a specified number of experimental conditions based on model disagreement.
 
-    Args:
-        num: number of experimental conditions to select
-        all_conditions: The pool to sample from.
-            Attention: `conditions` is a field of the standard state
-        models: The sampler might use output from the theorist.
-            Attention: `models` is a field of the standard state
-        reference_conditions: The sampler might use reference conditions
+        This function generates a pool of conditions by evaluating the disagreement among 
+        multiple models and then randomly samples a specified number of conditions from 
+        this pool.
 
-    Returns:
-        Sampled pool of experimental conditions
-    """
+        Args:
+            all_conditions (Union[pd.DataFrame, np.ndarray]): The pool to sample from.
+                This can be a DataFrame or a NumPy array containing all possible experimental conditions.
+            models (List): A list of models used to evaluate disagreement. The models should 
+                be compatible with the `model_disagreement_sample` function.
+            reference_conditions (Union[pd.DataFrame, np.ndarray], optional): Reference conditions 
+                that might be used by the sampler to guide the sampling process. Defaults to None.
+            num (int, optional): Number of experimental conditions to select. Defaults to 1.
+
+        Returns:
+            pd.DataFrame: A DataFrame containing the sampled experimental conditions.
+        """
+    # Generate a pool of conditions based on model disagreement
     conditions = model_disagreement_sample(
           all_conditions,
           models,
-          num_samples=10
+          num_samples=10 # Number of samples to generate from model disagreement
         )
+    # Randomly sample `num` conditions from the generated pool
     return conditions.sample(n=num)
